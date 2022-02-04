54ef82a4946c89414d0617f32ece934abc98fdd4
calvinballing <calvinballing@gmail.com>
2022-02-04 13:16:16 -0500

    init

---
+++ src/constants/orthography.ts
@@ -0,0 +1,36 @@
+import { CONFIG } from './config'
+
+export const ORTHOGRAPHY = [
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+]
+
+if (CONFIG.normalization) {
+  ORTHOGRAPHY.forEach(
+    (val, i) => (ORTHOGRAPHY[i] = val.normalize(CONFIG.normalization))
+  )
+}
